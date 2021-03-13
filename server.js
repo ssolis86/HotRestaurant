29547d4524c00f247628f3c4bd0ec9196120b828
Michael Jones <mtjones1979@yahoo.com>
2021-03-13 11:04:45 -0600

    basic server

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const express = require('express');
 
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
+
+    
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+    app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, "tables.html")));
+    app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, "reserve.html")));
+
+    app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
