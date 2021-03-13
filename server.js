27252bb283e31962c371c54653e26729f4330ea8
Stephen Solis <ssolis8619@gmail.com>
2021-03-13 10:09:22 -0600

    server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
