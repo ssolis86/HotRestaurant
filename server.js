4a2718b29e2f5b11b3214f10a58947e0d725521c
Michael Jones <mtjones1979@yahoo.com>
2021-03-13 13:12:17 -0600

    updated java script

--- server.js
+++ server.js
@@ -7,35 +7,36 @@ const express = require('express');
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
 
-    const tables = [
+    const reservation = [
         {
             name: 'Angelica',
             email: 'angelica@bootcamp.com',
-            phoneNumber: '512-123-4567',
+            number: '512-123-4567',
             uniqueId: 1,
         },
         {
             name: 'Drew',
             email: 'drew@bootcamp.com',
-            phoneNumber: '512-234-4567',
+            number: '512-234-4567',
             uniqueId: 2,
         },
         {
             name: 'Stephen',
             email: 'stephen@bootcamp.com',
-            phoneNumber: '512-654-4567',
+            number: '512-654-4567',
             uniqueId: 3,
         },
         {
             name: 'Michael',
             email: 'michael@bootcamp.com',
-            phoneNumber: '512-987-5689',
+            number: '512-987-5689',
             uniqueId: 4,
         }
     ]
     
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-    app.get('/view', (req, res) => res.sendFile(path.join(__dirname, "view.html")));
+    app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, "tables.html")));
     app.get('/make', (req, res) => res.sendFile(path.join(__dirname, "make.html")));
 
+    app.get('/reservation', (req, res) => res.json(reservations));
     app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
